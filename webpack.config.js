330aea5c35edd3bbe0ebcc7fedc9ca4b5e5acc58
dev-miguel-resources <ingenieromiguelch@gmail.com>
2023-02-09 13:39:59 -0300

    personalization webpack config part. 2

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 const path = require("path");
-const webpack = require("webpack");
+//const webpack = require("webpack"); // plugins
 
 module.exports = {
   // mode: defines el entorno para el cual estamos configurando el build  
-  mode: "production",
+  mode: "production", // none, // development, production
   // entry: apuntas al archivo de entrada a la aplicación
   entry: "./src/index.js",
   // output: defines donde alojarás los archivos estáticos generados y puedes personalizar el nombre del archivo estático del js
@@ -22,8 +22,71 @@ module.exports = {
           presets: ["@babel/preset-env", "@babel/preset-react"],
         },
       },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      /*{
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },*/
+      {
+        test: /\.(gif|png|jpe?g)$/i,
+        use: [
+          "file-loader",
+          {
+            loader: "image-webpack-loader",
+            options: {
+              bypassOnDebug: true, // primero, leerá el recurso y verá si es un recurso seguro y lo sanitiza
+              disable: true, // no lea el recurso de forma inmediata
+            }
+          }
+        ] 
+      },
+      /*{
+        test: /\.svg$/,
+        use: ["@svgr/webpack"],
+      },
+      /*{
+        test: /\.pdf$/i,
+        use: [
+          "file-loader",
+          {
+            loader: "pdf-loader"
+          }
+        ] 
+      },
+      /*{
+        test: /\.txt$/i,
+        use: [
+          "file-loader",
+          {
+            loader: "raw-loader"
+          }
+        ] 
+      },*/
     ],
   },
+  // plugins: son características que me permiten darle soporte a mi configuración con webpack
+  plugins: [
+
+  ],
+  // resolve: es donde dejo registradas todas las extensiones que estoy resolviendo de cara al build
+  resolve: {
+
+  },
+  // me permite habilitar recomendaciones, manejo de errores, warnings, etc para añadir configuraciones y mejorar mi build en temas de performance
+  performance: {
+
+  },
+  // me permite crear y configurar un servidor proxy reverso para saber como se está comportando mi proyecto que se va ir a producción a partir del build
+  devServer: {
+
+  },
   // devtool: es el inspector de recomendaciones y errores en la generación del build
+  // devtool development
+  //devtool: "eval-cheap-module-source-map",
   devtool: "source-map",
+  // hot:
+  // static paths:
 };
