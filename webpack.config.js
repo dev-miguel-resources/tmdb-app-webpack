e0b59bf179b50b3a13bccd474ef74c68291ca805
dev-miguel-resources <ingenieromiguelch@gmail.com>
2023-02-14 10:47:16 -0300

    webpack definitions

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require("path");
-const webpack = require("webpack"); // plugins
+//const webpack = require("webpack");
 
 module.exports = {
   // mode: defines el entorno para el cual estamos configurando el build
@@ -77,11 +77,9 @@ module.exports = {
     ],
   },
   // plugins: son características que me permiten darle soporte a mi configuración con webpack
-  plugins: [
-    new webpack.ProvidePlugin({
-      React: "react",
-    }),
-  ],
+  /*plugins: [
+    
+  ],*/
   // resolve: es donde dejo registradas todas las extensiones que estoy resolviendo de cara al build
   resolve: {
     extensions: [".js", ".jsx", ".css", ".png"],
